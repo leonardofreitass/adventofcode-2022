35bb923a6fb8a4dcd4dfb646c959946e9912eb72
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2022-12-01 09:54:42 +0100

    feat: day 1 part 2

--- src/adventofcode_2022/exercises/day_1/part_2.clj
+++ src/adventofcode_2022/exercises/day_1/part_2.clj
@@ -1,4 +1,13 @@
 (ns adventofcode-2022.exercises.day-1.part-2)
 
 (defn run
-  [inputs])
+  [inputs]
+  (reduce + (nth 
+    (reduce
+      (fn [[acc total] cal]
+        (if (= cal "")
+          [0 (vec (take 3 (sort > (conj total acc))))]
+          [(+ acc (Integer/parseInt cal)) total]))
+      [0 [0 0 0]]
+      inputs)
+    1)))
