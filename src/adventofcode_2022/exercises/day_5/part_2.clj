6e79a0caa41e2945c8f18f1c86a1ab1658e29a67
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2022-12-05 11:52:36 +0100

    feat: day 5

---
+++ src/adventofcode_2022/exercises/day_5/part_2.clj
@@ -0,0 +1,29 @@
+(ns adventofcode-2022.exercises.day-5.part-2)
+(require '[clojure.string :as str :refer [includes? blank?]])
+
+(defn parse-stacks
+  [initial-stacks]
+  (vec (map
+    (fn [v] (filter #(not= (str %) " ") v))
+    (apply mapv vector (map 
+      (fn [line]
+        (map #(nth % 1) (partition 4 4 [" "] line))) 
+      initial-stacks)))))
+
+(defn run
+  [inputs]
+  (let [initial-stacks (filter #(includes? % "[") inputs)
+        cmds (drop (+ 2 (count initial-stacks)) inputs)]
+     (apply str (map
+      #(first %)
+      (reduce
+        (fn [stacks cmd]
+          (let [[n _from _to] (map #(Integer/parseInt %) (re-seq #"\d+" cmd))
+                from (dec _from)
+                to (dec _to)]
+            (assoc stacks
+              from (drop n (nth stacks from))
+              to (concat (take n (nth stacks from)) (nth stacks to)))))
+        (parse-stacks initial-stacks)
+        cmds)))))
+
