72f34ec1addf102f468d15b4e640ae668fa77bc1
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2022-12-06 10:49:11 +0100

    feat: day 6

---
+++ src/adventofcode_2022/exercises/day_6/part_2.clj
@@ -0,0 +1,19 @@
+(ns adventofcode-2022.exercises.day-6.part-2)
+
+(defn queue
+  ([coll]
+    (reduce conj clojure.lang.PersistentQueue/EMPTY coll)))
+
+(def window-size 14)
+
+(defn run
+  [inputs]
+  (loop [n window-size
+         window (queue (take window-size (seq (first inputs))))
+         rest (drop window-size (seq (first inputs)))]
+    (if (= (count (set window)) window-size)
+      n
+      (recur
+        (inc n)
+        (pop (conj window (first rest)))
+        (next rest)))))
